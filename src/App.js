27047e3b09825245d655779732543d159f758e3c
Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>
2022-05-14 00:49:26 +0530

    Categories section added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import './categories.style.scss';
+const App = () => {
+    const categories= [
+      {
+        id: 1,
+        title: 'Processors',
+
+      },
+      {
+        id: 2,
+        title: 'GPUs',
+
+      },
+      {
+        id: 3,
+        title: 'Motherboards',
+
+      },
+      {
+        id: 4,
+        title: 'Power Supply Units',
+
+      },
+      {
+        id: 5,
+        title: 'Cabinets',
+
+      },
+    ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({title, id, imageURL}) => (
+      <div className="category-container">
+        <div className="background-image"/>
+        {/* img */}
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+       )) }
     </div>
   );
 }
